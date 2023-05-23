8bf36bff2d88733488eff9c957b679f05eee743d
dev-praise <praise.adeoti20@gmail.co>
2023-05-23 16:22:47 +0100

    b. smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,15 @@
+
+async function main() {
+ 
+  const Token = await ethers.getContractFactory("Token")
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log(`token deployed to address: ${token.address}`)
+ 
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
