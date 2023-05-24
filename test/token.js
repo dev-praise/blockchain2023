0b99127823366094ef731a174c46f0cdeaeb9377
dev-praise <praise.adeoti20@gmail.co>
2023-05-24 22:40:44 +0100

    c. test contract

---
+++ test/token.js
@@ -0,0 +1,41 @@
+const {ethers} = require('hardhat');
+const {expect} = require('chai');
+
+const weitoken = (n) => {return ethers.utils.parseUnits(n.toString(),'ether')}
+
+
+describe("Token", () => {
+
+	let token
+
+	beforeEach( async () => {
+		const Token = await ethers.getContractFactory('Token');
+		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
+	})
+
+	describe('Deployment', () => {
+
+		const name = 'Dapp University'
+		const symbol = 'DAPP'
+		const decimal = '18'
+		const totalSupply = weitoken(1000000)
+
+		it('has correct name', async () => {
+			expect(await token.name()).to.equal(name);
+		})
+
+		it('has correct symbol', async () => {
+			expect(await token.symbol()).to.equal(symbol);
+		})
+
+		it('has correct decimal', async () =>{
+			expect(await token.decimal()).to.equal(decimal);
+		})
+
+		it ('has correct totalSupply', async () =>{
+			expect(await token.totalSupply()).to.equal(totalSupply);			
+		})
+
+	})
+	
+})
