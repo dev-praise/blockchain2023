c7deb827d55c26d241bcd813431208bd006335ff
dev-praise <praise.adeoti20@gmail.co>
2023-06-02 11:36:11 +0100

    e. transfer token

--- test/token.js
+++ test/token.js
@@ -2,6 +2,7 @@ const {ethers} = require('hardhat');
 const {expect} = require('chai');
 
 const weitoken = (n) => {return ethers.utils.parseUnits(n.toString(),'ether')}
+const totalSupply = weitoken(1000000)
 
 
 describe("Token", () => {
@@ -14,6 +15,7 @@ describe("Token", () => {
 	
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})
 
 	describe('Deployment', () => {
@@ -21,7 +23,7 @@ describe("Token", () => {
 		const name = 'Dapp University'
 		const symbol = 'DAPP'
 		const decimal = '18'
-		const totalSupply = weitoken(1000000)
+	  //const totalSupply = weitoken(1000000)
 
 		it('has correct name', async () => {
 			expect(await token.name()).to.equal(name);
@@ -43,6 +45,59 @@ describe("Token", () => {
 		it ('it assigns totalSupply to deployer', async () =>{
 			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);			
 		})
+	})
+
+	describe('Sending tokens', () => {
+		let amount, transaction, results
+
+		describe ('success', () => {
+
+			beforeEach( async() => {
+				amount = weitoken(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				results = await transaction.wait()
+			})
+
+			it ('transfers token',  async () => {
+
+				expect(await token.balanceOf(deployer.address)).to.equal(weitoken(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
+
+			it('emits transfer event', async() =>{
+
+				const event = results.events[0]
+				expect(event.event).to.be.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+
+
+			})
+
+		})
+
+		describe ('failure', () => {
+
+			it ('rejects invalid amount', async () =>{
+
+				const invalidAmount = weitoken(1000000000)
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+
+			it ('rejects wrong address', async () => {
+
+				const amount = weitoken(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+
+
+			})
+
+		})
+
+
 	})
 	
 })
